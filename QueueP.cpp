26818f44fe31304c241abffba33e93746bd9572c
Leshenka1 <zubelaleksey@gmail.com>
2022-03-07 19:29:02 +0300

    add Queue

---
+++ QueueP.cpp
@@ -0,0 +1,238 @@
+﻿#include "QueueP.h"
+
+QueueP::QueueP()
+{
+	front = nullptr;
+	for (int i = 0; i < count_p; i++)
+	{
+		rears[i] = nullptr;
+		priority_size[i] = 0;
+	}
+}
+
+QueueP::QueueP(const QueueP& other)
+{
+	front = nullptr;
+	for (int i = 0; i < count_p; i++)
+	{
+		rears[i] = nullptr;
+		priority_size[i] = 0;
+	}
+	clone(other);
+}
+
+bool QueueP::empty() const
+{
+	return (front == nullptr);
+}
+
+int QueueP::get_front_element() const
+{
+	if (!empty()) {
+		return front->value;
+	}
+	else {
+		throw exception("error: it's empty\n");
+	}
+}
+
+string QueueP::get_front_priority() const
+{
+	if (!empty()) {
+		if (int(front->priority) == 0) {
+			return "LOW";
+		}
+		else if (int(front->priority) == 1) {
+			return "MEDIUM";
+		}
+		else if (int(front->priority) == 2) {
+			return "HIGH";
+		}
+	}
+	else {
+		throw exception("error: it's empty\n");
+	}
+}
+
+void QueueP::erase()
+{
+	while (pop())
+	{
+
+	}
+}
+
+QueueP& QueueP::operator=(const QueueP& other)
+{
+	if (&other == this)
+	{
+		return *this;
+	}
+	else
+	{
+		erase();
+		clone(other);
+		return *this;
+	}
+}
+
+QueueP::QueueP(QueueP&& tmp) noexcept
+{
+	replace(front, tmp.front);
+	for (int i = 0; i < count_p; i++)
+	{
+		replace(rears[i], tmp.rears[i]);
+		priority_size[i] = tmp.priority_size[i];
+	}
+}
+
+QueueP& QueueP::operator=(QueueP&& other) noexcept
+{
+	if (&other == this)
+	{
+		return *this;
+	}
+	replace(front, other.front);
+	for (int i = 0; i < count_p; i++)
+	{
+		replace(rears[i], other.rears[i]);
+		priority_size[i] = other.priority_size[i];
+	}
+	return *this;
+}
+
+int QueueP::get_length() const
+{
+	int totalSize = 0;
+	for (int i = 0; i < count_p; i++)
+	{
+		totalSize += priority_size[i];
+	}
+	return totalSize;
+}
+
+void QueueP::replace(QPItem*& a, QPItem*& b)
+{
+	a = b;
+	b = nullptr;
+}
+
+bool QueueP::pop()
+{
+	if (get_length() > 0)
+	{
+		QPItem* temp = front;
+		front = front->next;
+		priority_size[int(temp->priority)]--;
+		if (rears[int(temp->priority)] == temp)
+		{
+			rears[int(temp->priority)] = nullptr;
+		}
+		delete temp;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int QueueP::get_priority_length(PRIORITY priority) const
+{
+	return priority_size[int(priority)];
+}
+
+void QueueP::insert(int value, PRIORITY priority)
+{
+	if (int(priority) >= count_p || int(priority) < 0)
+	{
+		throw exception("error: incorrect priority\n");
+	}
+	QPItem* newItem = new QPItem(value, priority, nullptr);
+	if (empty())
+	{
+		front = newItem;
+	}
+	else
+	{
+		if (priority > front->priority)
+		{
+			newItem->next = front;
+			front = newItem;
+		}
+		else
+		{
+			if (get_priority_length(priority) != 0)
+			{
+				QPItem* rear = rears[int(priority)];
+				newItem->next = rear->next;
+				rear->next = newItem;
+			}
+			else
+			{
+				QPItem* rearMin = front;
+				for (int i = int(priority) + 1; i < count_p; i++)
+				{
+					if (rears[i] != nullptr)
+					{
+						rearMin = rears[i];
+						break;
+					}
+				}
+				rearMin->next = newItem;
+				QPItem* rearNext = nullptr;
+				if (int(priority) >= 1)
+				{
+					rearNext = rears[int(priority) - 1];
+				}
+				newItem->next = rearNext;
+			}
+		}
+
+	}
+	rears[int(priority)] = newItem;
+	priority_size[int(priority)]++;
+}
+
+void QueueP::insert(QPItem* qpItem)
+{
+	insert(qpItem->value, qpItem->priority);
+}
+
+void QueueP::clone(const QueueP& other)
+{
+	if (other.get_length() > 0)
+	{
+		QPItem* temp = other.front;
+		while (temp->next != nullptr)
+		{
+			insert(temp);
+			temp = temp->next;
+		}
+	}
+}
+
+ostream& QueueP::write_on_stream(ostream& s)
+{
+	if (!empty())
+	{
+		QPItem* tmp = front;
+		while (tmp != nullptr) {
+			s << tmp->value << endl;
+			if (tmp->next != nullptr) {
+				s << "," << endl;
+			}
+			tmp = tmp->next;
+		}
+	}
+	else
+	{
+		s << "EMPTY.\n";
+	}
+	return s;
+}
+
+QueueP::~QueueP()
+{
+	erase();
+}
---
+++ main.cpp
@@ -0,0 +1,176 @@
+﻿#include "QueueP.h"
+using namespace std;
+
+#include <conio.h>
+
+const int ESC = 27;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	PRIORITY temp_p;
+
+	char working_queue = 0;
+
+	QueueP* temp = new QueueP();
+	QueueP* queue1 = new QueueP();
+	QueueP* queue2 = new QueueP();
+
+	while (true)
+	{
+		int value;
+		int priority;
+		char action = 0;
+
+		cout << "Главное меню :\n" <<
+			"Нажать 1 - Выбрать очередь для работы.\n" <<
+			"Нажать 2 - Обменять значения очередей.\n" <<
+			"Нажать Esc - Выйти из консольного приложения.\n";
+		action = _getche();
+		switch (action)
+		{
+		case '1':
+			system("cls");
+			cout << "Меню очередей :\n"
+				<< "Ввести 1 - Выбрать очередь №1\n"
+				<< "Ввести 2 - Выбрать очередь №2\n"
+				<< "Ввести Esc - Перейти в главное меню\n";
+			working_queue = _getche();
+			switch (working_queue)
+			{
+			case '1':
+				system("cls");
+				temp = queue1;
+				break;
+			case '2':
+				system("cls");
+				temp = queue2;
+				break;
+			case ESC:
+				system("cls");
+				temp = queue2;
+				break;
+			default:
+				cout << "Неверный номер.\n";
+				system("pause");
+				system("cls");
+				break;
+			}
+			if (working_queue == '1' || working_queue == '2')
+			{
+
+				cout << "Меню операций с очередью :\n"
+					<< "Ввод 1 - Проверка очереди на пустоту.\n"
+					<< "Ввод 2 - Получить информацию о голове очереди.\n"
+					<< "Ввод 3 - Удаление последнего элемента(pop).\n"
+					<< "Ввод 4 - Вставка элемента.\n"
+					<< "Ввод 5 - Получение общего количества элементов.\n"
+					<< "Ввод 6 - Получени информации о конкретном приоритете.\n"
+					<< "Ввод Esc - Возвращение в главное меню.\n";
+
+				action = _getche();
+				switch (action)
+				{
+				case '1':
+					cout << "\n";
+					temp->write_on_stream(cout);
+					system("pause");
+					system("cls");
+					break;
+				case '2':
+					cout << "\n";
+					try
+					{
+						cout << temp->get_front_element() << " ";
+						cout << temp->get_front_priority() << endl;
+					}
+					catch (exception e)
+					{
+						cout << e.what();
+					}
+					system("pause");
+					system("cls");
+					break;
+				case '3':
+					temp->pop();
+					cout << "Операция выполнена.\n";
+					system("pause");
+					system("cls");
+					break;
+				case '4':
+					system("cls");
+					cout << "Введите значение элемента для вставки.\n";
+					cin >> value;
+					cout << "Выберите приоритет данного элемента\nВведите 1 - для выбора LOW\nВведите 2 - для выбора MEDIUM\nВведите 3 - для выбора HIGH\n";
+					cin >> priority;
+					if (priority == 1) temp_p = PRIORITY::LOW;
+					else if (priority == 2) temp_p = PRIORITY::MEDIUM;
+					else if (priority == 3) temp_p = PRIORITY::HIGH;
+					else
+					{
+						cout << "Приоритет выбран неверно\n";
+						system("pause");
+						system("cls");
+						break;
+					}
+					temp->insert(value, temp_p);
+					cout << "Элемент вставлен.\n";
+					system("pause");
+					system("cls");
+					break;
+				case '5':
+					system("cls");
+					cout << "Общее количество элементов = " << temp->get_length() << endl;
+					system("pause");
+					system("cls");
+					break;
+				case '6':
+					system("cls");
+					cout << "Выберите приоритет по которому хотите получить информацию\nВведите 1 - для выбора LOW\nВведите 2 - для выбора MEDIUM\nВведите 3 - для выбора HIGH\n";
+					cin >> priority;
+					if (priority == 1) temp_p = PRIORITY::LOW;
+					else if (priority == 2) temp_p = PRIORITY::MEDIUM;
+					else if (priority == 3) temp_p = PRIORITY::HIGH;
+					else
+					{
+						cout << "Неверный приоритет\n";
+						system("pause");
+						system("cls");
+						break;
+					}
+					cout << "Количество элементов с данным приоритетом = " << temp->get_priority_length(temp_p);
+					system("pause");
+					system("cls");
+					break;
+				case ESC:
+					system("cls");
+					break;
+				default:
+					break;
+				}
+			}
+			else
+			{
+				break;
+			}
+			break;
+
+		case '2':
+			temp = new QueueP(move(*queue1));
+			*queue1 = move(*queue2);
+			*queue2 = move(*temp);
+			cout << "Успешно выполнено.\n";
+			system("pause");
+			system("cls");
+			break;
+
+		case ESC:
+			cout << "HРабота завершена.";
+			return 0;
+			break;
+		default:
+			break;
+		}
+	}
+}
